6d24a00b1a9dab178764d951410a83923a74272b
VishnuS2006 <vishnusekar2022@gmail.com>
2026-07-17 18:20:40 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        List<Integer> set = new ArrayList<>();
+        for(int num : nums){
+            if(set.contains(num)){
+                return true ;
+            }
+            else{
+                set.add(num);
+            }
+        }
+        return false ;
+    }
+}
